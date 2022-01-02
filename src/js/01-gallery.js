28e81d1e3f3f367120669374e8ba93d21a4e444f
Babak Anastasiya <dp080791bap@gmail.com>
2022-01-02 23:30:11 +0200

    video and form

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector ('.gallery');
+const imagesMarkup = createGalleryItemsMarkup (galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryItemsMarkup (galleryItems) {
+   return galleryItems.map (({preview, original, description})=>{
+        return `
+        <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} title=${description} />
+</a>`
+    }).join('');
+}
+let gallery = new SimpleLightbox('.gallery a', {doubleTapZoom: 1.5, captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(()=>{player.getCurrentTime().then(sec => {localStorage.setItem('videoplayer-current-time',JSON.stringify(sec))})},1000))
+const videoStart = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+console.log(videoStart);
+player.setCurrentTime(videoStart)
