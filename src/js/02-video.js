fd81bef413651a1e1c6a6e248052efa405d8585e
Babak Anastasiya <dp080791bap@gmail.com>
2022-01-05 22:35:54 +0200

    update video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,28 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import throttle from "lodash/throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-player.on(
-    'timeupdate',
-    throttle(() => {
-      player.getCurrentTime().then(sec => {
-        localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
-      });
-    }, 1000),
-  );
-  
-  const timeToStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
-  player.setCurrentTime(timeToStart);
+player.on('timeupdate', throttle(videoStart, 1000)); 
+
+function videoStart(timeToStart) {
+    console.log(timeToStart); 
+    localStorage.setItem("videoplayer-current-time", timeToStart.seconds);
+}; 
+
+const stopTime = localStorage.getItem("videoplayer-current-time");
+console.log(stopTime);
+
+player.setCurrentTime(stopTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
